605cb02e4ad261e4c3047fc1f0f60680cb96e194
Birowsky <birowsky@gmail.com>
2014-04-21 15:02:11 +0200

    Improve Analytics

--- app/modules/analytics/analytics.js
+++ app/modules/analytics/analytics.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('round')
-.service('rAnalytics', function Analytics($window, $rootScope, $location, roundSett) {
+.service('rAnalytics', function Analytics($window,
+																					$rootScope,
+																					$location,
+																					roundSett	) {
 	var that = this;
+
+	initialize()
 
 	//google analytics
 	var ga = $window.ga;
@@ -54,6 +59,20 @@ angular.module('round')
 			exDescription: msg + '\n ○ ' + trace
 		});
 	};
+
+	function initialize () {
+		(function(i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function() {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+	}
 
 
 });
