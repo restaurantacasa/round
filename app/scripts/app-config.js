147d5a8030293b20ba673d3f7a74563f11cf77cf
Birowsky <birowsky@gmail.com>
2014-04-21 23:26:07 +0200

    app-config configures the app-sett

--- app/scripts/app-config.js
+++ app/scripts/app-config.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('round')
-.config(function () {
-
+.config(function (RoundSett) {
+	RoundSett.analytics.GoogleAnalytics.accounts = [
+		{
+			name: 'RoundAnalytics',
+			id: 'UA-36888310-3'
+		}
+	];
 });
--- app/scripts/app-sett.js
+++ app/scripts/app-sett.js
@@ -4,12 +4,7 @@ angular.module('round')
 .constant('RoundSett', {
 	analytics: {
 		GoogleAnalytics: {
-			accounts: [
-				{
-					name: 'RoundAnalytics',
-					id: 'UA-36888310-3'
-				}
-			]
+			accounts: [	]
 		}
 	}
 });
