3e1cfb1aa899c03b826f4d0bdbea9e5d76a841af
Birowsky <birowsky@gmail.com>
2014-04-21 05:17:12 +0200

    Create roundSett

---
+++ app/scripts/services/round-sett.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('round')
+.constant('roundSett', {
+	analytics: {
+		GoogleAnalytics: {
+			accounts: [
+				{
+					name: 'SecondAnalytics',
+					id: 'UA-36888310-3'
+				}
+			]
+		}
+	}
+});
