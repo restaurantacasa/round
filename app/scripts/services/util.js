b75d15a56c373c8ca18433194ee1e0c126d3ef04
Birowsky <birowsky@gmail.com>
2014-08-15 03:26:37 +0200

    Fix util

--- app/scripts/services/util.js
+++ app/scripts/services/util.js
@@ -14,16 +14,16 @@ angular.module('round')
 		if (angular.isArray(members)) {
 			angular.forEach(members, function (memberName) {
 				if (typeof suspect[memberName] === 'undefined') {
-					throw 'missing argument member: ' + memberName;
+					throw new Error('missing argument member: ' + memberName);
 				}
 			});
 		} else if (angular.isObject(members)) {
 			angular.forEach(members, function (type, memberName) {
 				if (typeof suspect[memberName] !== type) {
-					throw 'missing or bad argument member type: ' +
+					throw new Error('missing or bad argument member type: ' +
 						memberName +
 						', type: ' +
-						type;
+						type);
 				}
 			});
 		}
