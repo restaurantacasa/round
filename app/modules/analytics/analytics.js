244600ccd5bce19ca261d435a497f3c50d51952a
Birowsky <birowsky@gmail.com>
2014-12-26 15:11:27 +0100

    Refactor and add support for ecommerce in analytics

--- app/modules/analytics/analytics.js
+++ app/modules/analytics/analytics.js
@@ -10,32 +10,24 @@ angular.module('round')
 
 	var that = this;
 
+	var sett = RoundSett.analytics.GoogleAnalytics;
 	var accounts;
 
 	initialize();
 
-	this.set = function () {
-		//convert args to array
-		var args = Array.prototype.slice.call( arguments, 0 );
 
-		_(accounts).each(function (account) {
-			var localArgs = angular.copy(args);
-			localArgs.unshift(account.name ? account.name + '.set' : 'set');
-			$window.ga.apply(that, localArgs);
-		});
+	this.set = function () {
+		runCommand('set', arguments);
 	};
 
 	this.send = function () {
-		//convert args to array
-		var args = Array.prototype.slice.call( arguments, 0 );
-
-		_(accounts).each(function (account) {
-			var localArgs = angular.copy(args);
-			localArgs.unshift(account.name ? account.name + '.send' : 'send');
-			$window.ga.apply(that, localArgs);
-		});
+		runCommand('send', arguments);
 	};
 
+	this.ecommerce = function () {
+		var args = _.toArray(arguments);
+		runCommand('ecommerce:' + args.shift(), args);
+	};
 
 	this.sendException = function (msg, trace) {
 		that.send('exception', {
@@ -59,24 +51,37 @@ angular.module('round')
 	/*-----  End of Shorthands  ------*/
 
 
+	function runCommand (command, argsObj) {
+		var args = _.toArray(argsObj);
+		_(accounts).each(function (account) {
+			var localArgs = angular.copy(args);
+			localArgs.unshift(
+				account.name ?
+				account.name + '.' + command :
+				command
+			);
+			$window.ga.apply(that, localArgs);
+		});
+	}
+
 
 
 	function initialize () {
 		(function(i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
+			i.GoogleAnalyticsObject = r;
 			i[r] = i[r] || function() {
-				(i[r].q = i[r].q || []).push(arguments)
+				(i[r].q = i[r].q || []).push(arguments);
 			}, i[r].l = 1 * new Date();
 			a = s.createElement(o),
 			m = s.getElementsByTagName(o)[0];
 			a.async = 1;
 			a.src = g;
-			m.parentNode.insertBefore(a, m)
+			m.parentNode.insertBefore(a, m);
 		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 		//pick non-duplicates from accounts
 		accounts =
-			_(RoundSett.analytics.GoogleAnalytics.accounts).uniq(function (account) {
+			_(sett.accounts).uniq(function (account) {
 			return account.id;
 		});
 
@@ -85,7 +90,11 @@ angular.module('round')
 			$window.ga('create', account.id, {
 				name: account.name
 			});
+			if (account.enableEcommerce) {
+				$window.ga(account.name +'.require', 'ecommerce');
+			}
 		});
+
 
 	}
 
