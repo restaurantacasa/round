8924dd48f2b0688aba34923f547792d0e640f129
Birowsky <birowsky@gmail.com>
2015-03-05 01:15:07 +0100

    Fix previous

--- app/scripts/app-is-scrolling.js
+++ app/scripts/app-is-scrolling.js
@@ -4,28 +4,29 @@ angular
 .module('round').run(function ($window) {
 
 	var windowEl = angular.element($window);
+	var htmlEl = angular.element('html');
 	var YES_SCROLL_CLASS = 'rIsScrolling';
 	var NO_SCROLL_CLASS = 'rNotScrolling';
 	var DELAY = 400;
 
 
-	function setNoScroll (element) {
-		element.addClass(NO_SCROLL_CLASS);
-		element.removeClass(YES_SCROLL_CLASS);
+	function setNoScroll () {
+		htmlEl.addClass(NO_SCROLL_CLASS);
+		htmlEl.removeClass(YES_SCROLL_CLASS);
 	}
-	function setYesScroll (element) {
-		element.addClass(YES_SCROLL_CLASS);
-		element.removeClass(NO_SCROLL_CLASS);
+	function setYesScroll () {
+		htmlEl.addClass(YES_SCROLL_CLASS);
+		htmlEl.removeClass(NO_SCROLL_CLASS);
 	}
 
 
 	var setNoScrollDebounced = _.debounce(setNoScroll, DELAY);
 
-	setNoScroll(windowEl);
+	setNoScroll();
 
 	windowEl.on('scroll', function () {
-		setYesScroll(windowEl);
-		setNoScrollDebounced(windowEl);
+		setYesScroll();
+		setNoScrollDebounced();
 	});
 
 
